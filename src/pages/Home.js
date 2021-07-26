bdb4cb60b4a72be1d0cea21b40f41af2944f30de
behemothoz <behemothoz@yandex.ru>
2021-07-26 21:59:47 +0300

    testing fonts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 const data = [
     { id: 1, original: 'original 1', translate: 'translate 2' },
     { id: 2, original: 'original 2', translate: 'translate 3' },
-]
+];
 
 export const Home = () => {
     return (
-        <ul className="p-3">
+        <ul className="py-2 px-3">
             {data.map(item => {
                 const { id, original, translate } = item;
 
-                return <li key={id}>{original} | {translate}</li>
+                return (
+                    <li key={id} className="py-1">
+                        <div className="p-3 bg-white">
+                            <span className="block" style={{ fontWeight: 600 }}>{original}</span>
+                            <span style={{ fontSize: 12 }}>{translate}</span>
+                        </div>
+                    </li>
+                );
             })}
         </ul>
     );
